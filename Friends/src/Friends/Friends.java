3f5e0ab93320f8e2b1c0bbfbe9c4b3304df93d15
delaros13 <danieldelarosa706@gmail.com>
2023-11-29 19:38:26 -0500

    OKks

---
+++ Friends/src/Friends/Friends.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Friends;
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.lang.NumberFormatException;
+/**
+ *
+ * @author Daniel
+ */
+public class Friends {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Formulario form = new Formulario();
+        form.setVisible(true);
+    }
+}
